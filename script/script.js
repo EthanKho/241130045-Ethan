ac9c073b7bc1fcc8355101f4bf4fc6e94211f298
Ethan Kho <ethan.kho@students.mikroskil.ac.id>
2025-07-09 19:05:24 +0700

    JS

---
+++ script/script.js
@@ -0,0 +1,108 @@
+document.querySelectorAll('card-produk').forEach(card => {
+    const input = card.querySelector('.quantity input');
+    const minus = card.querySelector('.minus');
+    const plus = card.querySelector(".plus");
+
+    plus.addEventListener('click', () => {
+        const harga = card.querySelector('.price');
+        const nominalHarga = harga.textContent.match(/¥d+/)[0];
+
+        let value = parseInt(input.value);
+        input.value = value + 1;
+
+        harga.innerHTML = `$${(nominalHarga / value) * input.value}`
+    });
+
+    minus.addEventListener('click', () => {
+        const harga = card.querySelector('.price');
+        const nominalHarga = harga.textContent.match(/¥d+/)[0];
+
+        let value = parseInt(input.value);
+        if (value > 1) {input.value = value - 1};
+
+        harga.innerHTML = `$${(nominalHarga / value) * input.value}`
+    });
+
+    const harga = card.querySelector('.price');
+    const nominalHarga = harga.textContent.match(/¥d+/)[0];
+
+    input.addEventListener('input', () => {
+        let value = parseInt(input.value);
+        if (isNaN(value) || value < 1) {
+            input.value = 1;
+        };
+
+        harga.innerHTML = `$${(nominalHargaAwal) * input.value}`
+    });
+});
+
+//Perilaku untuk kotak input kode promo
+const inputKodePromo = document.querySelector('#kodePromo');
+
+inputKodePromo.addEventListener('input', () => {
+    inputKodePromo.value = inputKodePromo.value.toUpperCase().replace(/¥s+/g, '');
+});
+
+//Reset & Apply Form, memunculkan ataupun menghapus output
+const orderForm = document.querySelector('.order-section form');
+const output = document.querySelector("#output");
+
+orderForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    output.style.display = 'block';
+
+    const outputList = document.querySelector("#output ol");
+    outputList.innerHTML = '';
+
+    //Memunculkan list output
+    let total = 0;
+    document.querySelectorAll('.card-produk').forEach(card => {
+        const judul = card.querySelector('h3').textContent;
+        const qty = card.querySelector('.quantity input').value;
+        const harga = card.querySelector('.price').textContent.match(/¥d+/)[0];
+
+        const li = document.createElement('li')
+
+        li.innerHTML = `<p><span id="judulProduk">${judul}</span><span id="subtotalHarga">$${harga}</span></p>`
+        outputList.appendChild(li);
+
+        total += parseInt(harga);
+    });
+
+    //Menghitung total awal dan total akhir setelah diskon
+    const totalHargaAwal = document.querySelector('#totalHargaAwal');
+    const totalHargaAkhir = document.querySelector('#totalHargaAkhir');
+
+    totalHargaAwal.style.color = 'black';
+    totalHargaAwal.style.textDecoration = 'none';
+    totalHargaAkhir.style.display = 'none';
+
+    totalHargaAwal.innerHTML = `$${total}`;
+
+    const kodePromo = document.querySelector('#kodePromo').value;
+
+    if(kodePromo) {
+        totalHargaAkhir.style.display = 'block';
+        totalHargaAwal.style.color = 'gray';
+        totalHargaAwal.style.textDecoration = 'line-through';
+
+        let diskon = 0;
+        if(kodePromo === 'DISKON50'){diskon = 0.5}
+        else if (kodePromo === 'DISKON20'){diskon = 0.2}
+        else{
+            totalHargaAwal.style.color = 'black';
+            totalHargaAwal.style.textDecoration = 'none';
+            totalHargaAkhir.style.display = 'none';
+        };
+        totalHargaAkhir.innerHTML = `$${total*(1-diskon)}`;
+
+    };
+});
+
+$('#reset').click(() => {
+    $('#output').css('display', 'none');
+
+    $('card-produk input').each((quantity) => {
+        $(quantity).val(1);
+    });
+});
